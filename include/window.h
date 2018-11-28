a305f0b0c87d54ab1a04c68d5e3a98631dbccb3b
Trenton Schulz <trentonw@ifi.uio.no>
2018-11-28 14:13:07 +0000

    Start connecting up the processes.

--- include/window.h
+++ include/window.h
@@ -31,15 +31,12 @@
 #define WINDOW_H
 
 #include <QtWidgets/QWidget>
+#include <QtCore/QProcess>
 #include <ros/ros.h>
+#include <vector>
 class QLabel;
-class QSpinBox;
-class QCheckBox;
-class QDoubleSpinBox;
-class QListView;
-class QModelIndex;
 class QPushButton;
-class QRadioButton;
+class QButtonGroup;
 
 class Window : public QWidget
 {
@@ -48,10 +45,18 @@ public:
   Window(QWidget* parent = 0);
   ~Window();
 
-//private slots:
+private slots:
+  void locationClicked(int negative_id);
+  void pythonProcessStateChanged(QProcess::ProcessState newState) const;
+  void stopProcessStateChanged(QProcess::ProcessState newState) const;
+  void pythonProcessFinished(int exitCode, QProcess::ExitStatus exitStatus);
+  void stopProcessFinished(int exitCode, QProcess::ExitStatus exitStatus);
+  void emergencyStop();
 
 private:
   void setupUi();
+  void disableLocationButtons(bool disable);
+  QButtonGroup *button_group_;
   QPushButton *kitchen_1_button_;
   QPushButton *kitchen_2_button_;
   QPushButton *dining_table_1_button_;
@@ -65,6 +70,9 @@ private:
   QLabel *ordering_label_3_;
   QLabel *ordering_label_4_;
   QLabel *current_location_label_;
+  QProcess python_process_;
+  QProcess stop_process_;
+  std::vector<QString> locations_;
   ros::NodeHandle nodeHandle_;
 };
 
--- src/window.cpp
+++ src/window.cpp
@@ -28,22 +28,34 @@
  ******************************************************************/
 
 #include "window.h"
+#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QCheckBox>
-#include <QtWidgets/QDialogButtonBox>
-#include <QtWidgets/QDoubleSpinBox>
-#include <QtWidgets/QFormLayout>
 #include <QtWidgets/QPushButton>
-#include <QtWidgets/QRadioButton>
-#include <QtWidgets/QVBoxLayout>
+#include <QtCore/QDebug>
 #include <algorithm>
 
 Window::Window(QWidget* parent)
   : QWidget(parent)
+  , python_process_(this)
+  , stop_process_(this)
+  , locations_({QString(QLatin1String("kitchen1")), QString(QLatin1String("kitchen2")),
+		QString(QLatin1String("dining_table1")), QString(QLatin1String("dining_table2")),
+		QString(QLatin1String("sofa1")), QString(QLatin1String("sofa2"))})
   , nodeHandle_(ros::NodeHandle())
 {
+
   setupUi();
+  connect(&python_process_, SIGNAL(stateChanged(QProcess::ProcessState)),
+	  this, SLOT(pythonProcessStateChanged(QProcess::ProcessState)));
+  connect(&stop_process_, SIGNAL(stateChanged(QProcess::ProcessState)),
+	  this, SLOT(stopProcessStateChanged(QProcess::ProcessState)));
+  connect(&python_process_, SIGNAL(finished(int, QProcess::ExitStatus)),
+	  this, SLOT(pythonProcessFinished(int, QProcess::ExitStatus)));
+  connect(&stop_process_, SIGNAL(finished(int, QProcess::ExitStatus)),
+	  this, SLOT(stopProcessFinished(int, QProcess::ExitStatus)));
+
+
 }
 
 static QLabel *createOrderingLabel()
@@ -68,28 +80,32 @@ static QPushButton *createLocationButton(const QString &buttonText, QButtonGroup
 
 void Window::setupUi()
 {
-  auto *layout = new QGridLayout();
-  auto *buttonGroup = new QButtonGroup(this);
-  buttonGroup->setExclusive(true);
+  button_group_ = new QButtonGroup(this);
+  button_group_->setExclusive(true);
   ordering_label_1_ = createOrderingLabel();
   ordering_label_2_ = createOrderingLabel();
   ordering_label_3_ = createOrderingLabel();
   ordering_label_4_ = createOrderingLabel();
   current_location_label_ = new QLabel(tr("Unknown"));
   current_location_label_->setAlignment(Qt::AlignCenter);
-  kitchen_1_button_ = createLocationButton(tr("Kitchen 1"), buttonGroup);
-  kitchen_2_button_ = createLocationButton(tr("Kitchen 2"), buttonGroup);
-  dining_table_1_button_ = createLocationButton(tr("Dining Table 1"), buttonGroup);
-  dining_table_2_button_ = createLocationButton(tr("Dining Table 2"), buttonGroup);
-  sofa_1_button_ = createLocationButton(tr("Sofa 1"), buttonGroup);
-  sofa_2_button_ = createLocationButton(tr("Sofa 2"), buttonGroup);
+  kitchen_1_button_ = createLocationButton(tr("Kitchen 1"), button_group_);
+  kitchen_2_button_ = createLocationButton(tr("Kitchen 2"), button_group_);
+  dining_table_1_button_ = createLocationButton(tr("Dining Table 1"), button_group_);
+  dining_table_2_button_ = createLocationButton(tr("Dining Table 2"), button_group_);
+  sofa_1_button_ = createLocationButton(tr("Sofa 1"), button_group_);
+  sofa_2_button_ = createLocationButton(tr("Sofa 2"), button_group_);
+
+  connect(button_group_, SIGNAL(buttonClicked(int)), SLOT(locationClicked(int)));
   emergency_stop_button_ = new QPushButton(tr("Emergency &Stop"));
   auto button_font = emergency_stop_button_->font();
   button_font.setWeight(QFont::Bold);
   emergency_stop_button_->setFont(button_font);
   emergency_stop_button_->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::MinimumExpanding));
+  connect(emergency_stop_button_, SIGNAL(clicked()), SLOT(emergencyStop()));
   next_location_button_ = new QPushButton(tr("Next Location"));
   next_location_button_->setSizePolicy(QSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Maximum));
+
+  auto *layout = new QGridLayout();
   layout->addWidget(ordering_label_1_, 0, 0);
   layout->addWidget(ordering_label_2_, 0, 1);
   layout->addWidget(ordering_label_3_, 0, 2);
@@ -117,3 +133,61 @@ Window::~Window()
 {
 }
 
+void Window::disableLocationButtons(bool disable)
+{
+  for (const auto &button : button_group_->buttons()) {
+    button->setDisabled(disable);
+  }
+}
+
+void Window::locationClicked(int negative_id)
+{
+  const auto id = abs(negative_id) - 2; // automatic numbering starts at -2
+  Q_ASSERT_X(id >= 0 && id < locations_.size(), "locationClicked", "index out of range");
+  Q_ASSERT_X(python_process_.state() == QProcess::NotRunning, "locationClicked", "Process is still running");
+  const auto& location = locations_.at(id);
+  const auto command = QLatin1String("rosrun");
+  const QStringList arguments({QLatin1String("uh_robots"), QLatin1String("move_base.py"),
+			    QLatin1String("-n"), location});
+  disableLocationButtons(true);
+  qDebug() << "Runnining" << command << arguments;
+  python_process_.start(command, arguments);
+
+}
+
+void Window::pythonProcessStateChanged(QProcess::ProcessState newState) const
+{
+  qDebug() << "python process Got new state" << newState;
+}
+
+void Window::stopProcessStateChanged(QProcess::ProcessState newState) const
+{
+  qDebug() << "stop got new state" << newState;
+}
+
+void Window::emergencyStop()
+{
+  Q_ASSERT_X(stop_process_.state() == QProcess::NotRunning, "emergencyStop", "Process is still running");
+  const QString command(QLatin1String("rostopic"));
+  const QStringList arguments({QLatin1String("pub"), QLatin1String("-1"),
+			       QLatin1String("/enable_software_runstop"),
+			       QLatin1String("std_msgs/Bool"),
+			       QLatin1String("\"data: true\"")});
+  emergency_stop_button_->setText(tr("Stopping…"));
+  emergency_stop_button_->setDisabled(true);
+  qDebug() << "Runnining" << command << arguments;
+  stop_process_.start(command, arguments);
+}
+
+void Window::pythonProcessFinished(int exitCode, QProcess::ExitStatus exitStatus)
+{
+  disableLocationButtons(false);
+  qDebug() << "Python finished" << exitCode << exitStatus;
+}
+
+void Window::stopProcessFinished(int exitCode, QProcess::ExitStatus exitStatus)
+{
+  emergency_stop_button_->setText(tr("Emergency Stop"));
+  emergency_stop_button_->setDisabled(false);
+  qDebug() << "Stop finished" << exitCode << exitStatus;
+}
