3c81248161071819c623d4bb2444189a2121b7ab
Trenton Schulz <trentonw@ifi.uio.no>
2018-12-13 15:51:42 +0000

    Clang-format plus add a timeout to the torso.
    In the unlikely case that the torso doesn't return, just quit after 10
    seconds. It should be much longer than necessary.

--- gui/src/fetch_process_controller.cpp
+++ gui/src/fetch_process_controller.cpp
@@ -30,7 +30,7 @@
 #include "fetch_process_controller.h"
 #include <ros/console.h>
 
-FetchProcessController::FetchProcessController(QObject *parent)
+FetchProcessController::FetchProcessController(QObject* parent)
   : QObject(parent)
   , base_process_(this)
   , torso_process_(this)
@@ -53,7 +53,7 @@ FetchProcessController::FetchProcessController(QObject *parent)
           SLOT(reconfigureProcessFinished(int, QProcess::ExitStatus)));
 }
 
-static void terminateIfRunning(QProcess &process)
+static void terminateIfRunning(QProcess& process)
 {
   process.terminate();
   process.waitForFinished();
@@ -63,7 +63,7 @@ FetchProcessController::~FetchProcessController()
 {
   // Let everything terminate without a cascade.
   // We are going down anyway, but we don't need to crash.
-  QSignalBlocker blocker(this); 
+  QSignalBlocker blocker(this);
   terminateIfRunning(torso_process_);
   terminateIfRunning(base_process_);
   terminateIfRunning(stop_process_);
@@ -88,9 +88,12 @@ void FetchProcessController::stopProcessStateChanged(QProcess::ProcessState newS
 void FetchProcessController::baseProcessFinished(int exitCode, QProcess::ExitStatus exitStatus)
 {
   ROS_DEBUG("Base finished exit code: %d ExitStatus: %d", exitCode, exitStatus);
-  if (!location_queue_.empty()) {
+  if (!location_queue_.empty())
+  {
     travelToNextStop();
-  } else {
+  }
+  else
+  {
     emit moveFinished();
   }
 }
@@ -107,16 +110,18 @@ void FetchProcessController::stopProcessFinished(int exitCode, QProcess::ExitSta
   emit stopFinished();
 }
 
-void FetchProcessController::travelToLocation(const QString &location)
+void FetchProcessController::travelToLocation(const QString& location)
 {
-  Q_ASSERT_X(base_process_.state() == QProcess::NotRunning, "travelToLocation", "Process is still running");
+  Q_ASSERT_X(base_process_.state() == QProcess::NotRunning, "travelToLocation", "Process is still "
+                                                                                "running");
   location_queue_.push_back(location);
   travelToNextStop();
 }
 
-void FetchProcessController::travelToLocations(const std::vector<QString> &locations)
+void FetchProcessController::travelToLocations(const std::vector<QString>& locations)
 {
-  for (const auto &location : locations) {
+  for (const auto& location : locations)
+  {
     location_queue_.push_back(location);
   }
   travelToNextStop();
@@ -124,67 +129,68 @@ void FetchProcessController::travelToLocations(const std::vector<QString> &locat
 
 void FetchProcessController::travelToNextStop()
 {
-  Q_ASSERT_X(!location_queue_.empty(), "FetchProcesscontroller::travelToNextstop()", "location queue was empty, but we wanted to go to another location");
+  Q_ASSERT_X(!location_queue_.empty(), "FetchProcesscontroller::travelToNextstop()",
+             "location queue was empty, but we wanted to go to another location");
   const auto location = location_queue_.front();
   location_queue_.pop_front();
   const QString command(QLatin1String("rosrun"));
-  const QStringList arguments(
-    { QLatin1String("uh_robots"),
-      QLatin1String("move_base.py"),
-      QLatin1String("base"),
-      QLatin1String("-n"), location,
-      QLatin1String("--timeout"),
-      QLatin1String("60")});
-  ROS_DEBUG("Running %s %s", command.toUtf8().constData(), arguments.join(", ").toUtf8().constData());
+  const QStringList arguments({ QLatin1String("uh_robots"), QLatin1String("move_base.py"),
+                                QLatin1String("base"), QLatin1String("-n"), location,
+                                QLatin1String("--timeout"), QLatin1String("60") });
+  ROS_DEBUG("Running %s %s", command.toUtf8().constData(),
+            arguments.join(", ").toUtf8().constData());
   base_process_.start(command, arguments);
 }
 
 void FetchProcessController::emergencyStop()
 {
-  Q_ASSERT_X(stop_process_.state() == QProcess::NotRunning, "emergencyStop", "Process is still running");
+  Q_ASSERT_X(stop_process_.state() == QProcess::NotRunning, "emergencyStop", "Process is still "
+                                                                             "running");
   const QString command(QLatin1String("rostopic"));
-  const QStringList arguments({ QLatin1String("pub"), QLatin1String("-1"), QLatin1String("/enable_software_runstop"),
+  const QStringList arguments({ QLatin1String("pub"), QLatin1String("-1"),
+                                QLatin1String("/enable_software_runstop"),
                                 QLatin1String("std_msgs/Bool"), QLatin1String("data: true") });
-  ROS_DEBUG("Running %s %s", command.toUtf8().constData(), arguments.join(", ").toUtf8().constData());
+  ROS_DEBUG("Running %s %s", command.toUtf8().constData(),
+            arguments.join(", ").toUtf8().constData());
   stop_process_.start(command, arguments);
 }
 
 void FetchProcessController::moveTorso(double heightInMeters)
 {
-  if (torso_process_.state() != QProcess::NotRunning) {
+  if (torso_process_.state() != QProcess::NotRunning)
+  {
     ROS_DEBUG("Torso process still running, waiting for it to finish");
     torso_process_.waitForFinished();
   }
   const QString command(QLatin1String("rosrun"));
-  const QStringList arguments(
-    { QLatin1String("uh_robots"), QLatin1String("move_base.py"), QLatin1String("torso"), QLatin1String("-p"),
-      QString::number(heightInMeters) });
-  ROS_DEBUG("Running %s %s", command.toUtf8().constData(), arguments.join(", ").toUtf8().constData());
+  const QStringList arguments({ QLatin1String("uh_robots"), QLatin1String("move_base.py"),
+                                QLatin1String("torso"), QLatin1String("-p"),
+                                QString::number(heightInMeters), QLatin1String("--timeout"),
+                                QLatin1String("10") });
+  ROS_DEBUG("Running %s %s", command.toUtf8().constData(),
+            arguments.join(", ").toUtf8().constData());
   torso_process_.start(command, arguments);
 }
 
 void FetchProcessController::changeVelocityCurve(const QString& newCurve)
 {
-
-  if (torso_process_.state() != QProcess::NotRunning) {
+  if (torso_process_.state() != QProcess::NotRunning)
+  {
     ROS_DEBUG("base process still running, waiting for it to finish");
     torso_process_.waitForFinished();
   }
   const QString command(QLatin1String("rosrun"));
-  const QStringList arguments({QLatin1String("dynamic_reconfigure"),
-			       QLatin1String("dynparam"),
-			       QLatin1String("set"),
-			       QLatin1String("/move_base/SisoLocalPlanner"),
-			       QLatin1String("velocity_curve"),
-			       newCurve});
+  const QStringList arguments({ QLatin1String("dynamic_reconfigure"), QLatin1String("dynparam"),
+                                QLatin1String("set"), QLatin1String("/move_base/SisoLocalPlanner"),
+                                QLatin1String("velocity_curve"), newCurve });
 
-  ROS_DEBUG("Running %s %s", command.toUtf8().constData(), arguments.join(", ").toUtf8().constData());
+  ROS_DEBUG("Running %s %s", command.toUtf8().constData(),
+            arguments.join(", ").toUtf8().constData());
   reconfigure_process_.start(command, arguments);
-			       
 }
 
-
-void FetchProcessController::reconfigureProcessFinished(int exitCode, QProcess::ExitStatus exitStatus)
+void FetchProcessController::reconfigureProcessFinished(int exitCode,
+                                                        QProcess::ExitStatus exitStatus)
 {
   ROS_DEBUG("Reconfigure finished exit code: %d ExitStatus: %d", exitCode, exitStatus);
   emit velocityCurveChanged();
