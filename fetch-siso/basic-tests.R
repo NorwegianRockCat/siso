5e55132162ce77473af29ba6ca1ee78fbf321798
Trenton Schulz <trentonw@ifi.uio.no>
2019-01-29 11:46:33 +0000

    Try and check in my first R project.
    I'm not sure if certain things should be included, but we'll figure it
    out as we go along. However, I think I have some of the basics here
    for computing alpha and other things.

---
+++ fetch-siso/basic-tests.R
@@ -0,0 +1,25 @@
+# We are dependent on the pysch package
+library(psych)
+
+readFetchSisoData <- function() {
+    results <- read.csv("results.csv", header = TRUE)
+    results
+}
+
+# Return a list of psych alpha results, one for each movement thing
+listOfAlphasForSisoData <- function(resultsDataFrame) {
+    # Pull these out to make life a little easier
+    anthroFrame <- resultsDataFrame[6:10]
+    animacyFrame <- resultsDataFrame[12:16]
+    likeabilityFrame <- resultsDataFrame[17:21]
+    intelligenceFrame <- resultsDataFrame[22:27]
+    safetyFrame <- resultsDataFrame[28:30]
+
+    alphaAnthro <- alpha(anthroFrame)
+    alphaAnimacy <- alpha(animacyFrame)
+    alphaLikeability <- alpha(likeabilityFrame)
+    alphaInt <- alpha(intelligenceFrame)
+    alphaSafety <- alpha(safetyFrame, c("GSS1.1"))
+    listOfAlphas <- list(anthro=alphaAnthro, animacy=alphaAnimacy, likeability=alphaLikeability, int=alphaInt, safety=alphaSafety)
+    listOfAlphas
+}
