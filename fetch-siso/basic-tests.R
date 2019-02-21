0a3e37c0480129b9924f6e936c634a6851878533
Trenton Schulz <trentonw@ifi.uio.no>
2019-02-21 14:01:48 +0100

    Make this test work for either t-test or Wilcox Signed Rank
    Now we are starting to get something.

--- fetch-siso/basic-tests.R
+++ fetch-siso/basic-tests.R
@@ -782,8 +782,8 @@ calculateAverages <- function(df) {
                          ) %>% dplyr::select(ID, Age, Gender, Experience.Robots, !!!tws.AverageVariableNames)
 }
 
-testbyName <- function(df, myName, alternative = c("two.sided", "less", "greater")) {
+nonparamTestByName <- function(df, myName, func = c(t.test, wilcox.test), alternative = c("two.sided", "less", "greater")) {
     var.name <- quo_name(enquo(myName))
     vars <- dplyr::select(df, paste("Siso.", var.name, sep = ''),  paste("Linear.", var.name, sep = ''))
-    t.test(vars[[1]], vars[[2]], paired = TRUE, alternative)
+    func(vars[[1]], vars[[2]], paired = TRUE, alternative)
 }
